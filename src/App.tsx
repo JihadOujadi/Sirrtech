1af1ed520995537a2637a95417778169828cb80b
JihadOujadi <jihad_oujadi@yahoo.fr>
2025-10-23 14:38:02 +0200

    intégration hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Header from "./Blocs/Header";
+import HeroBanner from "./Blocs/HeroBanner";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
 
   return (
     <main className="sirrtech__main">
-      <Header></Header>
+      <section className="sirrtech__hero">
+        <Header></Header>
+        <HeroBanner></HeroBanner>
+      </section>
     </main>
   );
 }
---
+++ src/Blocs/HeroBanner.tsx
@@ -0,0 +1,13 @@
+function HeroBanner() {
+  return (
+    <section className="sirrtech__hero-banner">
+      <div className="sirrtech__hero-banner__title-container">
+        <h1 className="sirrtech__hero-banner__title">
+          Automatisez. Innovez. Accélérez
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default HeroBanner;
